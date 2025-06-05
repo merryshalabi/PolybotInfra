f4ede13864866746a544dea5525ae301023c6763
merryshalabi <merryshalabi59@gmail.com>
2025-06-05 13:46:14 +0300

    Fix file path in GitHub Actions workflow for dev NGINX deployment

--- scripts/deploy_nginx.sh
+++ scripts/deploy_nginx.sh
@@ -9,5 +9,5 @@ docker run -d \
   -p 443:443 \
   -p 8443:8443 \
   -v /home/ubuntu/conf.d:/etc/nginx/conf.d/ \
-  -v /etc/letsencrypt/live/merry-bot.fursa.click:/etc/nginx/certs:ro \
+  -v /etc/letsencrypt/live/merry-bot.fursa.click:/etc/nginx/certs/ \
   nginx
