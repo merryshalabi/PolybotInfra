4526a8556c5531f52e92f715ad9092650970dc18
merryshalabi <merryshalabi59@gmail.com>
2025-07-05 20:59:06 +0300

    add GitHub Actions pipelines to provision cluster and initialize control plane

--- tf/modules/k8s-cluster/variables.tf
+++ tf/modules/k8s-cluster/variables.tf
@@ -40,7 +40,7 @@ variable "max_size" {
   type        = number
   description = "Maximum number of worker nodes in ASG"
   default     = 3
-}\
+}
 variable "acm_cert_arn" {
   description = "ARN of the ACM certificate for the HTTPS listener"
   type        = string
